c673fc0fb2a2170217bb82860d210b2da3d9675a
Vishal Gupta <vishalg8897@gmail.com>
2018-07-20 15:24:29 +0530

    Bug Fixes

--- Ex1/CaesarCipher.java
+++ Ex1/CaesarCipher.java
@@ -2,51 +2,37 @@
 package caeser_cipher;
 import java.util.Scanner;
 
-public class Caeser_cipher {
-
-    /**
-     * @param args the command line arguments
-     */
+public class CaeserCipher {
     public static void main(String[] args) {
-        // TODO code application logic here
-         Scanner scan=new Scanner(System.in);
+        Scanner scan=new Scanner(System.in);
           
         System.out.println("Enter the message (lower Case, without spaces): ");
         String msg=scan.nextLine(); 
         char[] crypt=new  char[msg.length()];
            
-         System.out.println("Enter the key value (displacement): ");  
-         int key=scan.nextInt();
-         System.out.println("ENCRYPTED :");  
+        System.out.println("Enter the key value (displacement): ");  
+        int key=scan.nextInt();
+        System.out.println("ENCRYPTED :");  
         for(int i=0;i<msg.length();i++)
         {
-          
             if(msg.charAt(i)+key>122)
-            {
                 crypt[i]=(char)(96+key%26);
-            }
             else
-            {
-            crypt[i]=(char)(msg.charAt(i)+key);
-            }
-            System.out.print(crypt[i]);
-                
+                crypt[i]=(char)(msg.charAt(i)+key);
+            System.out.print(crypt[i]);                
         }
-        System.out.println();
-        System.out.println("DECRYPTED : "); 
-          for(int i=0;i<msg.length();i++)
-        {
-           
+        System.out.println("\nDECRYPTED : "); 
+        for(int i=0;i<msg.length();i++)
+        {           
             if(crypt[i]-key<97)
-            {crypt[i]=(char)(crypt[i]+26-key);}
+                crypt[i]=(char)(crypt[i]+26-key);
             else
-            crypt[i]=(char) (crypt[i]-key);
+                crypt[i]=(char) (crypt[i]-key);
             System.out.print(crypt[i]);
         }
     }
 
     private static boolean Equals(char charAt, String _) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet.");
     }
-    
 }
