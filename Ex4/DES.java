322dc0807af413826b122b91673e4fb0ce04596b
Vishal Gupta <vishalg8897@gmail.com>
2018-08-04 16:14:28 +0530

    Finished encryption of DES.

--- Ex4/DES.java
+++ Ex4/DES.java
@@ -235,10 +235,46 @@ class DES {
       int chosen_num = chosed_S[row_num][col_num];
       return int2bits(chosen_num, 4);
     }
+    public static String encrypt(String message, boolean[][] keys) {
+
+
+      int[][] IP_matrix = new int[][] {
+        {58,50,42,34,26,18,10,2},
+        {60,52,44,36,28,20,12,4},
+        {62,54,46,38,30,22,14,6},
+        {64,56,48,40,32,24,16,8},
+        {57,49,41,33,25,17,9,1},
+        {59,51,43,35,27,19,11,3},
+        {61,53,45,37,29,21,13,5},
+        {63,55,47,39,31,23,15,7}
+      };
+      int[][] IP_inv_matrix = new int[][]{
+        {40,8,48,16,56,24,64,32},
+        {39,7,47,15,55,23,63,31},
+        {38,6,46,14,54,22,62,30},
+        {37,5,45,13,53,21,61,29},
+        {36,4,44,12,52,20,60,28},
+        {35,3,43,11,51,19,59,27},
+        {34,2,42,10,50,18,58,26},
+        {33,1,41,9,49,17,57,25}
+      };
+      boolean[] M = binstr2bits(message);
+      boolean[] IP = permute(M, IP_matrix);
+
+      boolean[][] L = new boolean[17][32];
+      boolean[][] R = new boolean[17][32];
+
+      L[0] = Arrays.copyOfRange(IP, 0, 32);
+      R[0] = Arrays.copyOfRange(IP, 32, IP.length);
+
+      for (int i =1;i<17;i++){
+          L[i] = R[i-1];
+          R[i] = arrayXOR(L[i-1],feistel(R[i-1],keys[i-1]));
+      }
+      return bits2hex(permute(concat(R[16], L[16]),IP_inv_matrix));
     }
-    
     public static void main(String[] args) {
         boolean[][] Keys = keyGen("00010011 00110100 01010111 01111001 10011011 10111100 11011111 11110001");
-        bits2str(Keys[0]);
+        System.out.println(encrypt("0000 0001 0010 0011 0100 0101 0110 0111 1000 1001 1010 1011 1100 1101 1110 1111",Keys).toUpperCase());
     }
 }
