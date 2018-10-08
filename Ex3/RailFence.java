0aaaf65b5a6169867cc9aab72c861bd7644730e2
Vishal Gupta <vishalg8897@gmail.com>
2018-10-09 00:50:34 +0530

    Added RailFence comments

--- Ex3/RailFence.java
+++ Ex3/RailFence.java
@@ -1,3 +1,4 @@
+// Refer http://practicalcryptography.com/ciphers/rail-fence-cipher/
 import static java.lang.Math.abs;
 import java.util.Scanner;
 
@@ -8,40 +9,47 @@ class RailFence {
         Scanner reader = new Scanner(System.in);
         Scanner keyread = new Scanner(System.in);
         
-        System.out.println("\nEnter the key: ");
+        System.out.println("Enter the key : ");
         key = keyread.nextInt();
         
-        System.out.println("\nEnter the plain text: ");
+        System.out.println("\nEnter the message : ");
         String message = reader.nextLine();
         len = message.length();
-        System.out.println(len);
-        System.out.println(key);
+
+        // Initializing RailFence Cipher matrix
         char matrix [][] = new char [key][len];
         for (i=0;i<len;i++){
             for (j=0;j<key;j++)
                 matrix[j][i]='*';
         }
+        
+        // Setting elements of RailFence matrix
         char[] a = message.toCharArray();
         int ind;
         int tkey = key - 1;
         for (i=0;i<len;i++){
+            // Col = array index. Row needs to be calculated.
             ind = tkey - Math.abs(tkey-i%(2*tkey));
             matrix[ind][i]=a[i];
         }
+
+        System.out.println("\nRailFence Matrix :");
         for (i=0;i<key;i++){
             for (j=0;j<len;j++)
                 System.out.print(matrix[i][j]);
             System.out.print("\n");
         }
-        System.out.println("ENCRYPTED :");
+
+        // Print Encrypted message
+        System.out.println("\nENCRYPTED :");
         for (i=0;i<key;i++){
             for (j=0;j<len;j++)
-                System.out.print(matrix[i][j]);
-        }
-        System.out.println("\n\nDECRYPTION :");
-        System.out.println("\nEnter the key for decryption: ");
-        int dec_key = keyread.nextInt();
-        tkey = dec_key -1;
+                if (matrix[i][j]!='*')
+                    System.out.print(matrix[i][j]);
+        }        
+        
+        System.out.println("\n\nDECRYPTED :");
+        tkey = key -1;
         for (i=0;i<len;i++){
             ind = tkey - Math.abs(tkey-i%(2*tkey));
             System.out.print(matrix[ind][i]);
