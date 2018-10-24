847b810c0839805900a3cff986e92ac31899de1c
Vishal Gupta <vishalg8897@gmail.com>
2018-10-24 08:09:14 +0530

    Update MD5.java

--- Ex7/MD5.java
+++ Ex7/MD5.java
@@ -1,98 +1,155 @@
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.Scanner;
 
-public class MD5 {
-    private static final int   INIT_A     = 0x67452301;
-    private static final int   INIT_B     = (int) 0xEFCDAB89L;
-    private static final int   INIT_C     = (int) 0x98BADCFEL;
-    private static final int   INIT_D     = 0x10325476;
-    private static final int[] SHIFT_AMTS = { 7, 12, 17, 22, 5, 9, 14, 20, 4,
-            11, 16, 23, 6, 10, 15, 21    };
-    private static final int[] TABLE_T    = new int[64];
+public class md5 {
+
+    private static int[] H = new int[4];
+    private static final int[] SHIFT = {7, 12, 17, 22, 5, 9, 14, 20, 4, 11, 16, 23, 6, 10, 15, 21};
+    private static final int[] K = new int[64];
+
     static {
         for (int i = 0; i < 64; i++)
-            TABLE_T[i] = (int) (long) ((1L << 32) * Math.abs(Math.sin(i + 1)));
+            K[i] = (int) (long) ((1L << 32) * Math.abs(Math.sin(i + 1)));
     }
- 
-    public static byte[] computeMD5(byte[] message) {
-        int messageLenBytes = message.length;
-        int numBlocks = ((messageLenBytes + 8) >>> 6) + 1;
-        int totalLen = numBlocks << 6;
-        byte[] paddingBytes = new byte[totalLen - messageLenBytes];
-        paddingBytes[0] = (byte) 0x80;
-        long messageLenBits = (long) messageLenBytes << 3;
+
+    public static void process(byte[] block) {
+        int[] M = new int[16];
+
+        //separating 32 bits
+        for (int i = 0; i < 16; i++) {
+            byte[] temp = new byte[4];
+            System.arraycopy(block, 4 * i, temp, 0, 4);
+            int start = 0, end = 3;
+            while (start < end) {
+                byte t = temp[start];
+                temp[start] = temp[end];
+                temp[end] = t;
+                start++;
+                end--;
+            }
+            //from 32 bits
+            M[i] = ByteBuffer.wrap(temp).getInt();
+            System.out.println(Integer.toUnsignedString(M[i]));
+        }
+
+        int a = H[0], b = H[1], c = H[2], d = H[3];
+        int i;
+        for (i = 0; i < 64; i++) {
+            int f, g;
+            if (i < 16) {
+                f = (b & c) | (~b & d);
+                g = i;
+            } else if (i < 32) {
+                f = (d & b) | (~d & c);
+                g = (5 * i + 1) % 16;
+            } else if (i < 48) {
+                f = b ^ c ^ d;
+                g = (3 * i + 5) % 16;
+            } else {
+                f = c ^ (b | ~d);
+                g = (7 * i) % 16;
+            }
+            f = f + a + K[i] + M[g];
+            a = d;
+            d = c;
+            c = b;
+            b = b + Integer.rotateLeft(f, SHIFT[4 * (i / 16) + (i % 4)]);
+
+            System.out.print("Round " + (i + 1) + ": ");
+            printBuffer(a,b,c,d);
+        }
+        H[0] += a;
+        H[1] += b;
+        H[2] += c;
+        H[3] += d;
+        System.out.print("HASH : ");
+        printBuffer(H);
+    }
+
+    public static void getMD5(byte[] m) {
+        H[0] = 0x67452301;
+        H[1] = 0xefcdab89;
+        H[2] = 0x98badcfe;
+        H[3] = 0x10325476;
+        int byte_length = m.length;
+        int bits_length = byte_length * 8;
+        int append_bits = 448 - (bits_length % 512);
+        if (append_bits < 0)
+            append_bits += 512;
+        if (append_bits == 0)
+            append_bits = 512;
+        int append_bytes = append_bits / 8;
+        byte[] pad = new byte[append_bytes];
+
+        pad[0] = (byte) 0x80;
+        for (int i = 1; i < append_bytes; i++)
+            pad[i] = 0x00;
+        byte[] l_pad = new byte[8];
         for (int i = 0; i < 8; i++) {
-            paddingBytes[paddingBytes.length - 8 + i] = (byte) messageLenBits;
-            messageLenBits >>>= 8;
+            l_pad[i] = (byte) bits_length;
+            bits_length = bits_length / 256;
         }
-        int a = INIT_A;
-        int b = INIT_B;
-        int c = INIT_C;
-        int d = INIT_D;
-        int[] buffer = new int[16];
-        for (int i = 0; i < numBlocks; i++) {
-            int index = i << 6;
-            for (int j = 0; j < 64; j++, index++)
-                buffer[j >>> 2] = ((int) ((index < messageLenBytes) ? message[index] : paddingBytes[index - messageLenBytes]) << 24) | (buffer[j >>> 2] >>> 8);
-            int originalA = a;
-            int originalB = b;
-            int originalC = c;
-            int originalD = d;
-            for (int j = 0; j < 64; j++) {
-                int div16 = j >>> 4;
-                int f = 0;
-                int bufferIndex = j;
-                switch (div16) {
-                    case 0:
-                        f = (b & c) | (~b & d);
-                        break;
-                    case 1:
-                        f = (b & d) | (c & ~d);
-                        bufferIndex = (bufferIndex * 5 + 1) & 0x0F;
-                        break;
-                    case 2:
-                        f = b ^ c ^ d;
-                        bufferIndex = (bufferIndex * 3 + 5) & 0x0F;
-                        break;
-                    case 3:
-                        f = c ^ (b | ~d);
-                        bufferIndex = (bufferIndex * 7) & 0x0F;
-                        break;
-                }
-                int temp = b + Integer.rotateLeft(a + f + buffer[bufferIndex] + TABLE_T[j], SHIFT_AMTS[(div16 << 2) | (j & 3)]);
-                a = d;
-                d = c;
-                c = b;
-                b = temp;
-            }
-            a += originalA;
-            b += originalB;
-            c += originalC;
-            d += originalD;
+        byte[] padded_m = new byte[byte_length + append_bytes + 8];
+        System.arraycopy(m, 0, padded_m, 0, m.length);
+        System.arraycopy(pad, 0, padded_m, m.length, pad.length);
+        System.arraycopy(l_pad, 0, padded_m, m.length + pad.length, 8);
+
+        System.out.println("The padded message is ");
+
+        for (int i = 0; i < 64; i++) {
+            System.out.print(padded_m[i] + " ");
         }
-        byte[] md5 = new byte[16];
-        int count = 0;
+        System.out.println();
+
+        int no = padded_m.length / 64;
+
+        for (int i = 0; i < no; i++) {
+            byte[] block = new byte[64];
+            System.arraycopy(padded_m, i * 64, block, 0, 64);
+            process(block);
+        }
+
+    }
+
+    public static void main(String args[]) {
+        String hash = "";
+        String text = "";
+        System.out.println("Enter the text to hash :");
+        Scanner sc = new Scanner(System.in);
+        text = sc.nextLine();
+        System.out.println("The hash is ");
+        getMD5(text.getBytes());
+    }
+
+    private static void printBuffer(int[] H) {
         for (int i = 0; i < 4; i++) {
-            int n = (i == 0) ? a : ((i == 1) ? b : ((i == 2) ? c : d));
-            for (int j = 0; j < 4; j++) {
-                md5[count++] = (byte) n;
-                n >>>= 8;
-            }
+            ByteBuffer byteBuffer = ByteBuffer.allocate(4);
+            byteBuffer.order(ByteOrder.LITTLE_ENDIAN);
+            byteBuffer.putInt(H[i]);
+            int littleEndian = ByteBuffer.wrap(byteBuffer.array()).getInt();
+            System.out.print(Integer.toHexString(littleEndian));
         }
-        return md5;
+        System.out.println();
     }
- 
-    public static String toHexString(byte[] b) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < b.length; i++) {
-            sb.append(String.format("%02X", b[i] & 0xFF));
+
+    private static void printBuffer(int a,int b,int c,int d) {
+        for (int i = 0; i < 4; i++) {
+            ByteBuffer byteBuffer = ByteBuffer.allocate(4);
+            byteBuffer.order(ByteOrder.LITTLE_ENDIAN);
+            if(i==0)
+            byteBuffer.putInt(a);
+            if(i==1)
+            byteBuffer.putInt(b);
+            if(i==2)
+            byteBuffer.putInt(c);
+            if(i==3)
+            byteBuffer.putInt(d);
+            int littleEndian = ByteBuffer.wrap(byteBuffer.array()).getInt();
+            System.out.print(Integer.toHexString(littleEndian));
         }
-        return sb.toString();
-    }
- 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter Plain Text: ");
-        String s = in.nextLine();
-        System.out.println(toHexString(computeMD5(s.getBytes())));
+        System.out.println();
     }
 }
+
+
